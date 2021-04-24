6ca6ab0fdb1d5ce4b44c74245a0cc9f8481f1590
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-24 14:32:30 +0300

    Add files via upload

---
+++ HataTurleri/HataTurleri/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HataTurleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sayi1 = textBox1.Text;
+                string sayi2 = textBox2.Text;
+                int s1 = Convert.ToInt32(sayi1);
+                int s2 = Convert.ToInt32(sayi2);
+                int toplam = s1 + s2;
+                textBox3.Text = toplam.ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message+ "Sayi Girmelisiniz");
+
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(ex.Message+ "Daha kucuk bir sayi giriniz");
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                toplam += i;
+                
+            }
+            textBox3.Text = toplam.ToString();
+        }
+    }
+}
